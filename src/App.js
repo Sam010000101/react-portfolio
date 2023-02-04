d2ca4d85b862300ba070890dfbb9309c627cc5b1
Sam B <reposme@outlook.com>
2023-02-04 20:33:12 +0000

    Mass add to update repo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Project from './pages/Project';
-import Gallery from './pages/Gallery';
+
 
 function App() {
   return (
---
+++ src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import './style.css';
+
+function Home(props) {
+    return (
+        <div>
+            <Hero>
+
+            </Hero>
+
+            <Link>
+            </Link>
+        </div>
+    );
+}
+
+export default Home;
