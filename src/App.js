c699aebf58354ab95520e51761725386dd822c7a
Sam B <reposme@outlook.com>
2023-02-04 22:25:36 +0000

    Inclusion of imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Gallery from './pages/Gallery';
+import Project from './pages/Project';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar'
+import Wrapper from './components/Wrapper'
+import Footer from './components/Footer'
 
 
 function App() {
   return (
-<Home>
-
-</Home>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
