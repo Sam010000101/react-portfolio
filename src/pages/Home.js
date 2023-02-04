aa427c33c3bed94e8cf62405015b12ae52e009c7
Sam B <reposme@outlook.com>
2023-02-04 20:54:39 +0000

    Rearranged & renamed page files & removed unrequired style.css files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Hero from '../components/Hero';
-import './style.css';
+import Row from '../components/Row';
 
 function Home(props) {
     return (
-        <div>
-            <Hero>
 
-            </Hero>
+            <Row>            
 
-            <Link>
+            </Row>,
+            <Hero>,
+            </Hero>,
+
+            <Link>,
             </Link>
-        </div>
+
     );
 }
 
