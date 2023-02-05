725dad657b05f535a0e0115445dbafa6282b4eb4
Sam B <reposme@outlook.com>
2023-02-05 08:59:03 +0000

    Contact specific content added to page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,6 +3,7 @@ import Hero from '../components/Hero';
 import Container from "../components/Container";
 import Row from '../components/Row';
 import Col from "../components/Col";
+import Form from "../components/Form";
 import "./style.css"
 
 function Contact() {
@@ -15,35 +16,12 @@ function Contact() {
           <Container style={{ marginTop: 30 }}>
             <Row>
               <Col size="md-12">
-                <h1>Welcome to my portfolio!</h1>
+                <h1>Contact</h1>
               </Col>
             </Row>
             <Row>
               <Col size="md-12">
-                <h3>
-                  A little bit about me...
-                </h3>
-                <p>
-                  My interests
-      
-                </p>
-                <h3>
-                  My Experience...
-                </h3>
-                <p>
-                  <ul>CSS</ul>
-                  <ul>Bootstrap</ul>
-                  <ul>HTML</ul>
-                  <ul>Javascript</ul>
-                  <ul>JQuery</ul>
-                  <ul>Node JS</ul>
-                  <ul>and now React!</ul>
-      
-                </p>
-    <button>My GitHub</button> <button>My LinkedIn</button>
-    <button>My Resume</button>
-    <button>Call Me</button>
-    <button>Email Me</button>
+              <Form />
               </Col>
             </Row>
           </Container>
